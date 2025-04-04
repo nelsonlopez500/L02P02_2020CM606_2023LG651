b54de968e0467930c12ffab5716ab3f0ee9bf6bf
nelsonlopez500 <nelson.lopez@catolica.edu.sv>
2025-04-03 18:04:04 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2020CM606_2023LG651/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2020CM606_2023LG651.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
